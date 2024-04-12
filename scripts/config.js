48f5f872bfad7e9b22a51038e6a35148e93538fb
Gabriel <gabriel.luthun@gmail.com>
2024-04-12 03:06:33 +0200

    Ajout de mots supplémentaires (future amélioration : les mots seront générés aléatoirement)

--- scripts/config.js
+++ scripts/config.js
@@ -1,2 +1,2 @@
-const listeMots = ["Cachalot", "Pétunia", "Serviette", "Animal", "Bouquin", "Poisson", "Baleine", "Chat"]
-const listePhrases = ["Pas de panique", "La vie, l'univers et le reste", "Merci pour le poisson"]
+const listeMots = ["Bonjour", "Salut", "Serviette", "Chat", "Stylo", "Discord", "Développeur", "Azerty", "Clavier", "Souris", "Ordinateur", "Téléphone", "Pomme", "Banane", "Fraise", "Framboise", "Cerise", "Pêche", "Abricot", "Poire"]
+const listePhrases = ["Pas de panique", "La vie, l'univers et le reste", "Merci pour le poisson", ]
